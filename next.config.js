6f9cc60d58d9903de799f5d44788d323323dcc38
Hosam <hosamedgesayahi@gmail.com>
2023-12-11 16:20:26 +0330

    test:next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+const { hostname } = new URL("https://panel.oxinmart.com/");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [`${hostname}`]
+    domains: [`${hostname}`],
   },
 };
 
